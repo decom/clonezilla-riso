be03c86e13d9780ff6345b8379329202533ae7a7
Alain Andre <alainandreamaral@gmail.com>
2016-06-27 09:15:12 -0300

    Modularização da funcionalidade 'Criar nova tabela de particionamento'

---
+++ menu_tabela_particionamento.sh
@@ -0,0 +1,28 @@
+function menu_tabela_particionamento(){
+  source carregar_discos.sh
+  
+  local entradas_menu=""
+  for disco in $(carregar_discos)
+  do
+  local entradas_menu="$entradas_menu /dev/$disco /dev/$disco"
+  done
+ 
+  while : ; do
+    opcao=$(dialog --stdout                   \
+    --no-tags                                 \
+    --title "Discos Rigidos"                  \
+    --ok-label "Confirmar"                    \
+    --cancel-label "Cancelar"                 \
+    --menu "Escolha um Disco:"                \
+    0 0 0                                     \
+    $entradas_menu                            \
+    )
+    if [ -z $opcao ]; then
+        break
+    else
+    	 nome_tabela=$(dialog --stdout --inputbox 'Digite o nome para a tabela de particionamento:' 0 0)
+    	 sfdisk -d $opcao > $DIR_PARTICIONAMENTOS$nome_tabela
+    	 break
+    fi
+  done
+}
