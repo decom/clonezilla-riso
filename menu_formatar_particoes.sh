781df7fdbb0e77fdeeea344186295601d0ecd3ff
Alain Andre <alainandreamaral@gmail.com>
2016-06-27 09:04:28 -0300

    Modularização da funcionalidade 'Formatar partições'

---
+++ menu_formatar_particoes.sh
@@ -0,0 +1,40 @@
+function menu_formatar_particoes(){
+  source carregar_particoes.sh
+
+  local entradas_menu=""
+  for particao in $(carregar_particoes)
+  do
+  	local entradas_menu="$entradas_menu $particao on"
+  done
+  
+  while : ; do
+    local opcao=$(dialog --stdout       \
+    --no-items                          \
+    --title "Menu Formatar Partições"   \
+    --ok-label "Confirmar"              \
+    --cancel-label "Cancelar"           \
+    --checklist "Escolha as Partições:" \
+    0 0 0                               \
+    $entradas_menu                      \
+    )
+    if [ -z $opcao ]; then
+      break
+    fi
+    for item in $opcao
+    do
+	    tipo=$(blkid $item | sed 's/.*TYPE=//g' | cut -d'"' -f2)
+	    if [ $tipo -eq "ntfs" ]; then
+	      umount $item
+	      mkfs.ntfs -Fq $item
+	    fi
+	    if [ $tipo -eq "ext4" ]; then
+	      umount $item
+	      mkfs.ext4 -Fq $item
+	    fi
+	    if [ $tipo -eq "swap" ]; then
+	      umount $item
+	      mkswap $item
+	    fi
+	   done    
+  done
+}
