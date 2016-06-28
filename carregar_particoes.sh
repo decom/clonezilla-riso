a0de0f3eeccece8961cffdada4bcdd415945c574
Raylander <raylanderlopes@hotmail>
2016-06-28 10:14:41 -0300

    ALteração para modo padrão de variaveis

--- carregar_particoes.sh
+++ carregar_particoes.sh
@@ -1,16 +1,16 @@
 function carregar_particoes(){
   local particoes=$(blkid | cut -d':' -f1)
   local dispositivosComRepetidos=$(echo $particoes | sed -e 's/[0-9]*//g')
-  local dispositivosUnicosSemDev=$(for dispositivo in $dispositivosComRepetidos; do echo $dispositivo; done | uniq | sed -e 's/\/dev\///g')
+  local dispositivosUnicosSemDev=$(for dispositivo in $dispositivos_com_repetidos; do echo $dispositivo; done | uniq | sed -e 's/\/dev\///g')
   local dispositivosUSB=""
-  for dispositivo in $dispositivosUnicosSemDev
+  for dispositivo in $dispositivos_unicos_sem_dev
   do 
   	local usb=$(readlink -f /sys/class/block/${dispositivo}/device | grep usb);   
     if [ ! -z "$usb" ]; then 
-      local dispositivosUSB="$dispositivosUSB $dispositivo";   
+      local dispositivosUSB="$dispositivos_usb $dispositivo";   
     fi
   done
-  for dispositivo in $dispositivosUSB
+  for dispositivo in $dispositivos_usb
   do
     local particoes=$(for particao in $particoes; do echo $particao; done | grep -v $dispositivo)
   done
