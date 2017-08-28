1642de6ffed4b18dedc1d88ce814ea44dd72e3ba
decom <apoio@decom.cefetmg.br>
2017-08-28 11:35:50 -0300

    arquivo para trocar a UUID do windows

--- formata_windows_UUID.sh
+++ formata_windows_UUID.sh
@@ -5,8 +5,8 @@
 
 # Verifica os parâmetros do disco (ex:/dev/sda) e o arquivo de particionamento (ex: HD500)
 
-if [ $# -ne 2 ]; then
-   echo "Utilização: $0 [Disco] [Arquivo de Particionamento]";
+if [ $# -ne 1 ]; then
+   echo "Utilização: $0 [Disco]";
    exit 1;
 fi
 
